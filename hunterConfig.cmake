c84fa5ef0e22be9ddabaa3eb096c77f72e9e8ecc
morebtcg <651932351@qq.com>
2021-10-10 08:34:39 +0800

    Update framework, add MockTransactionalStorage

--- hunterConfig.cmake
+++ hunterConfig.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/688489b7603f02b0973bbaae4a22796c169971bc.tar.gz
-    SHA1 045746bb3fccd0c8a69f2d188ac4946d381901b2
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/afb584188a4ac1fb9a37e91367ff1399dff6c4c8.tar.gz
+    SHA1 ed05e99b239a25e4b1a41cb4b9e7bd6f1ec9b641
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON #DEBUG=ON
 ) 
