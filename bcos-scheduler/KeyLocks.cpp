2997403ec4a8cb7f2055f3dc8fac86968d239f56
morebtcg <651932351@qq.com>
2021-11-15 21:00:23 +0800

    Add keylock debug

--- bcos-scheduler/KeyLocks.cpp
+++ bcos-scheduler/KeyLocks.cpp
@@ -1,6 +1,7 @@
 #include "KeyLocks.h"
 #include "Common.h"
 #include <assert.h>
+#include <bcos-framework/libutilities/DataConvertUtility.h>
 #include <bcos-framework/libutilities/Error.h>
 #include <boost/throw_exception.hpp>
 
@@ -9,19 +10,33 @@ using namespace bcos::scheduler;
 bool KeyLocks::acquireKeyLock(
     const std::string_view& contract, const std::string_view& key, int64_t contextID, int64_t seq)
 {
+    SCHEDULER_LOG(TRACE) << "Acquire key lock, contract: " << contract << " key: " << toHex(key)
+                         << " contextID: " << contextID << " seq: " << seq;
+
     auto it = m_keyLocks.get<1>().find(std::tuple{contract, key});
     if (it != m_keyLocks.get<1>().end())
     {
         if (it->contextID != contextID)
         {
+            SCHEDULER_LOG(ERROR) << "Acquire key lock failed, request contextID: " << contextID
+                                 << " expected contextID: " << it->contextID;
+
             // Another context is owing the key
             return false;
         }
     }
 
+    SCHEDULER_LOG(TRACE) << "Acquire key lock success";
+
     // Current context owing the key, accquire it
-    m_keyLocks.get<1>().emplace(
+    [[maybe_unused]] auto [insertedIt, inserted] = m_keyLocks.get<1>().emplace(
         KeyLockItem{std::string(contract), std::string(key), contextID, seq});
+
+    if (!inserted)
+    {
+        SCHEDULER_LOG(ERROR) << "Insert key lock error!";
+        return false;
+    }
 
     return true;
 }
@@ -50,6 +65,16 @@ std::vector<std::string> KeyLocks::getKeyLocksByContract(
 
 void KeyLocks::releaseKeyLocks(int64_t contextID, int64_t seq)
 {
+    SCHEDULER_LOG(TRACE) << "Release key lock, contextID: " << contextID << " seq: " << seq;
+
     auto range = m_keyLocks.get<3>().equal_range(std::tuple{contextID, seq});
+
+    for (auto it = range.first; it != range.second; ++it)
+    {
+        SCHEDULER_LOG(TRACE) << "Releasing key lock, contract: " << it->contract
+                             << " key: " << toHex(it->key) << " contextID: " << it->contextID
+                             << " seq: " << it->seq;
+    }
+
     m_keyLocks.get<3>().erase(range.first, range.second);
 }
