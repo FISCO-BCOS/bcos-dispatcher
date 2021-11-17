d9657930778a01a4955654192085b8c28304f4e1
morebtcg <651932351@qq.com>
2021-11-17 21:25:45 +0800

    Update framework

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,6 +1,6 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/9d389de9dbb9b0826c34cd6ad58a0db115315e18.tar.gz
-    SHA1 b522077b82c2b90495b9769232267fe0b35050a8
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/fc305c3d284c2da622ab408040b0eeede24b2519.tar.gz
+    SHA1 7d0b7b67eb9299f2beccecab9a2b3003c6a38c8c
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
