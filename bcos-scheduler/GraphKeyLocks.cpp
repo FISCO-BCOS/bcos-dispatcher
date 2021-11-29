fa02f70891aa4c4ba407922eb26ab7b373856c55
morebtcg <651932351@qq.com>
2021-11-29 20:35:31 +0800

    Add graphlock

--- bcos-scheduler/GraphKeyLocks.cpp
+++ bcos-scheduler/GraphKeyLocks.cpp
@@ -17,7 +17,22 @@ using namespace bcos::scheduler;
 bool GraphKeyLocks::batchAcquireKeyLock(std::string_view contract,
     gsl::span<std::string const> keyLocks, int64_t contextID, int64_t seq)
 {
-    boost::ignore_unused(contract, keyLocks, contextID, seq);
+    if (!keyLocks.empty())
+    {
+        for (auto& it : keyLocks)
+        {
+            if (!acquireKeyLock(contract, it, contextID, seq))
+            {
+                auto message = (boost::format("Batch acquire lock failed, contract: %s"
+                                              ", key: %s, contextID: %ld, seq: %ld") %
+                                contract % toHex(it) % contextID % seq)
+                                   .str();
+                SCHEDULER_LOG(ERROR) << message;
+                BOOST_THROW_EXCEPTION(BCOS_ERROR(UnexpectedKeyLockError, message));
+                return false;
+            }
+        }
+    }
 
     return true;
 }
@@ -26,62 +41,101 @@ bool GraphKeyLocks::acquireKeyLock(
     std::string_view contract, std::string_view key, int64_t contextID, int64_t seq)
 {
     auto keyVertex = touchKey(contract, key);
-    auto [contextVertex, contextSeqVertex] = touchContext(contextID, seq);
-
-    auto [waitEdge, exists] = boost::edge(contextVertex, keyVertex, m_graph);
-    if (exists)
-    {
-        // Already waiting
-        return false;
-    }
+    auto contextVertex = touchContext(contextID);
 
     auto range = boost::out_edges(keyVertex, m_graph);
     for (auto it = range.first; it != range.second; ++it)
     {
-        auto contextVertex = it->m_target;
+        auto vertex = boost::get(VertexPropertyTag(), boost::target(*it, m_graph));
+        if (std::get<0>(*vertex) != contextID)
+        {
+            SCHEDULER_LOG(TRACE) << boost::format(
+                                        "Acquire key lock failed, request: [%s, %s, %ld, %ld] "
+                                        "exists: [%ld]") %
+                                        contract % key % contextID % seq % std::get<0>(*vertex);
+
+            // Key lock holding by another context
+            addEdge(contextVertex, keyVertex, seq);
+            return false;
+        }
     }
+
+    addEdge(keyVertex, contextVertex, seq);
+
+    SCHEDULER_LOG(TRACE) << "Acquire key lock success, contract: " << contract << " key: " << key
+                         << " contextID: " << contextID << " seq: " << seq;
 
     return true;
 }
 
 
-std::vector<std::string> GraphKeyLocks::getContractKeyLocksNotHoldingByContext(
+std::vector<std::string> GraphKeyLocks::getKeyLocksNotHoldingByContext(
     std::string_view contract, int64_t excludeContextID) const
 {
-    boost::ignore_unused(contract, excludeContextID);
-    return {};
+    std::set<std::string> uniqueKeyLocks;
+
+    auto range = boost::edges(m_graph);
+    for (auto it = range.first; it != range.second; ++it)
+    {
+        auto sourceVertex = boost::get(VertexPropertyTag(), boost::source(*it, m_graph));
+        auto targetVertex = boost::get(VertexPropertyTag(), boost::target(*it, m_graph));
+
+        if (sourceVertex->index() == 0 && std::get<0>(*sourceVertex) != excludeContextID &&
+            targetVertex->index() == 1 && std::get<0>(std::get<1>(*targetVertex)) != contract)
+        {
+            uniqueKeyLocks.emplace(std::get<1>(std::get<1>(*targetVertex)));
+        }
+    }
+
+    std::vector<std::string> keyLocks;
+    keyLocks.reserve(uniqueKeyLocks.size());
+    for (auto& it : uniqueKeyLocks)
+    {
+        keyLocks.emplace_back(std::move(it));
+    }
+
+    return keyLocks;
 }
 
 void GraphKeyLocks::releaseKeyLocks(int64_t contextID, int64_t seq)
 {
-    boost::ignore_unused(contextID, seq);
+    SCHEDULER_LOG(TRACE) << "Release key lock, contextID: " << contextID << " seq: " << seq;
+    auto vertex = touchContext(contextID);
+    boost::remove_in_edge_if(
+        vertex,
+        [this, seq](EdgeID edge) {
+            auto edgeSeq = boost::get(EdgePropertyTag(), edge);
+            if (edgeSeq == seq)
+            {
+                if (bcos::LogLevel::TRACE >= bcos::c_fileLogLevel)
+                {
+                    auto source = boost::get(VertexPropertyTag(), boost::source(edge, m_graph));
+                    const auto& [contract, key] = std::get<1>(*source);
+                    SCHEDULER_LOG(TRACE)
+                        << "Releasing key lock, contract: " << contract << " key: " << key;
+                }
+                return true;
+            }
+            return false;
+        },
+        m_graph);
 }
 
-auto GraphKeyLocks::detectDeadLock() const
+bool GraphKeyLocks::detectDeadLock() const
 {
     return false;
 }
 
-std::tuple<GraphKeyLocks::VertexID, GraphKeyLocks::VertexID> GraphKeyLocks::touchContext(
-    int64_t contextID, int64_t seq)
+GraphKeyLocks::VertexID GraphKeyLocks::touchContext(int64_t contextID)
 {
-    Vertex contextVertex(contextID);
-    auto [it, inserted] = m_vertexes.emplace(contextVertex, 0);
+    auto [it, inserted] = m_vertexes.emplace(Vertex(contextID), (VertexID)0);
     if (inserted)
     {
-        it->second = boost::add_vertex(m_graph);
+        it->second = boost::add_vertex(&(it->first), m_graph);
     }
     auto contextVertexID = it->second;
 
-    Vertex contextSeqVertex(std::make_tuple(contextID, seq));
-    std::tie(it, inserted) = m_vertexes.emplace(contextSeqVertex, 0);
-    if (inserted)
-    {
-        it->second = boost::add_vertex(m_graph);
-    }
-    auto contextSeqVertexID = it->second;
-
-    return {contextVertexID, contextSeqVertexID};
+    return contextVertexID;
 }
 
 GraphKeyLocks::VertexID GraphKeyLocks::touchKey(std::string_view contract, std::string_view key)
@@ -93,7 +147,42 @@ GraphKeyLocks::VertexID GraphKeyLocks::touchKey(std::string_view contract, std::
         return it->second;
     }
 
-    auto inserted =
-        m_vertexes.emplace_hint(it, Vertex(std::string(contract), std::string(key)), m_count++);
+    auto inserted = m_vertexes.emplace_hint(
+        it, Vertex(std::make_tuple(std::string(contract), std::string(key))), (VertexID)0);
+    inserted->second = boost::add_vertex(&(inserted->first), m_graph);
     return inserted->second;
+}
+
+void GraphKeyLocks::addEdge(VertexID source, VertexID target, int64_t seq)
+{
+    bool exists = false;
+
+    auto range = boost::edge_range(source, target, m_graph);
+    for (auto it = range.first; it != range.second; ++it)
+    {
+        auto edgeSeq = boost::get(EdgePropertyTag(), *it);
+        if (edgeSeq == seq)
+        {
+            exists = true;
+        }
+    }
+
+    if (!exists)
+    {
+        boost::add_edge(source, target, seq, m_graph);
+    }
+}
+
+void GraphKeyLocks::removeEdge(VertexID source, VertexID target, int64_t seq)
+{
+    auto range = boost::edge_range(source, target, m_graph);
+    for (auto it = range.first; it != range.second; ++it)
+    {
+        auto edgeSeq = boost::get(EdgePropertyTag(), *it);
+        if (edgeSeq == seq)
+        {
+            boost::remove_edge(*it, m_graph);
+            break;
+        }
+    }
 }
--- bcos-scheduler/GraphKeyLocks.h
+++ bcos-scheduler/GraphKeyLocks.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <tbb/concurrent_unordered_map.h>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/depth_first_search.hpp>
 #include <boost/graph/edge_list.hpp>
 #include <boost/graph/graph_selectors.hpp>
+#include <boost/graph/properties.hpp>
 #include <boost/graph/visitors.hpp>
 #include <any>
 #include <forward_list>
@@ -33,25 +33,86 @@ public:
     bool acquireKeyLock(
         std::string_view contract, std::string_view key, int64_t contextID, int64_t seq);
 
-    std::vector<std::string> getContractKeyLocksNotHoldingByContext(
+    std::vector<std::string> getKeyLocksNotHoldingByContext(
         std::string_view contract, int64_t excludeContextID) const;
 
     void releaseKeyLocks(int64_t contextID, int64_t seq);
 
-    auto detectDeadLock() const;
+    bool detectDeadLock() const;
+
+    struct Vertex : public std::variant<int64_t, std::tuple<std::string, std::string>>
+    {
+        using std::variant<int64_t, std::tuple<std::string, std::string>>::variant;
+
+        bool operator==(const std::tuple<std::string_view, std::string_view>& rhs) const
+        {
+            if (index() != 1)
+            {
+                return false;
+            }
+
+            auto view = std::make_tuple(std::string_view(std::get<0>(std::get<1>(*this))),
+                std::string_view(std::get<1>(std::get<1>(*this))));
+            return view == rhs;
+        }
+    };
 
 private:
-    using Graph = boost::adjacency_list<>;
-    using Vertex =
-        std::variant<std::tuple<int64_t, int64_t>, int64_t, std::tuple<std::string, std::string>>;
+    struct VertexIterator
+    {
+        using kind = boost::vertex_property_tag;
+    };
+    using VertexProperty = boost::property<VertexIterator, const Vertex*>;
+    struct EdgeSeq
+    {
+        using kind = boost::edge_property_tag;
+    };
+    using EdgeProperty = boost::property<EdgeSeq, int64_t>;
+
+    using Graph = boost::adjacency_list<boost::multisetS, boost::multisetS, boost::bidirectionalS,
+        VertexProperty, EdgeProperty>;
+    using VertexPropertyTag = boost::property_map<Graph, VertexIterator>::const_type;
+    using EdgePropertyTag = boost::property_map<Graph, EdgeSeq>::const_type;
     using VertexID = Graph::vertex_descriptor;
+    using EdgeID = Graph::edge_descriptor;
 
     Graph m_graph;
     std::map<Vertex, VertexID, std::less<>> m_vertexes;
 
-    std::tuple<GraphKeyLocks::VertexID, GraphKeyLocks::VertexID> touchContext(
-        int64_t contextID, int64_t seq);
+    VertexID touchContext(int64_t contextID);
     VertexID touchKey(std::string_view contract, std::string_view key);
+
+    void addEdge(VertexID source, VertexID target, int64_t seq);
+    void removeEdge(VertexID source, VertexID target, int64_t seq);
 };
 
 }  // namespace bcos::scheduler
+
+namespace std
+{
+inline bool operator<(const bcos::scheduler::GraphKeyLocks::Vertex& lhs,
+    const std::tuple<std::string_view, std::string_view>& rhs)
+{
+    if (lhs.index() != 1)
+    {
+        return false;
+    }
+
+    auto view = std::make_tuple(std::string_view(std::get<0>(std::get<1>(lhs))),
+        std::string_view(std::get<1>(std::get<1>(lhs))));
+    return view < rhs;
+}
+
+inline bool operator<(const std::tuple<std::string_view, std::string_view>& lhs,
+    const bcos::scheduler::GraphKeyLocks::Vertex& rhs)
+{
+    if (rhs.index() != 1)
+    {
+        return true;
+    }
+
+    auto view = std::make_tuple(std::string_view(std::get<0>(std::get<1>(rhs))),
+        std::string_view(std::get<1>(std::get<1>(rhs))));
+    return lhs < view;
+}
+}  // namespace std
