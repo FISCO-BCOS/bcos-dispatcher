014bdcd25b3440c895fd95cabb065d59772a8fcf
morebtcg <651932351@qq.com>
2021-12-07 15:56:32 +0800

    Update tars-protocol

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,12 +1,12 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/80345086f18586ef1e6122d7bf5eb9688e990472.tar.gz
-    SHA1 1fabfc8bb45d3793f5a4789a01f32fb35ec691e2
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/1003d35925569d25c84483aef4402556e6c74f18.tar.gz
+    SHA1 502eb233c2ce6f7c0e7e70b8bf64e50d25545c31
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
 hunter_config(bcos-tars-protocol
     VERSION 3.0.0-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-tars-protocol/archive/6bc78eee8426acca811d154ca87b213cf8ba859a.tar.gz
-    SHA1 38ca3f1606583cd25ec1a2ce6cb714a772f0f3ec
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-tars-protocol/archive/78d25fca2cc23eb0e082a1d381ebece6f6bff6fc.tar.gz
+    SHA1 f2ce87c145eabd792abc8190a8f26e9424f3d517
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON URL_BASE=${URL_BASE}
 )
